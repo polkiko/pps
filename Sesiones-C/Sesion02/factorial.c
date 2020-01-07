d0742546badb78dabfad93a3c2651df826961ac3
Jesus Jerez <jesus.jerez1@gmail.com>
2020-01-07 11:11:38 +0100

    Sesión 02 - Ejecutando C

--- Sesiones-C/Sesion02/factorial.c
+++ Sesiones-C/Sesion02/factorial.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * Las funciones en C son del tipo:
+ * tipo_return nombre(arg1, arg2...){
+ *
+ * }
+ * En C no existen métodos privados/publicos
+ *
+ */
 int factorial(int n){
   int f = 1;
   int i;
@@ -15,8 +23,18 @@ int main(int argc, char *argv[]){
   if(argc == 1){
     n = 0;
   } else {
-    n = atoi(argv[1]);
+    n = atoi(argv[1]); // La función atoi convierte la parte inicial del array
+                       // como un str a un int
   }
+
+  /**
+   * Para imprimir mensajes por pantalla utilizamos la funcion fprintf(), a la
+   * que le podemos añadir en el primer argumento la salida que queramos,
+   * siendo stderr la salida de error.
+   * Las variables se escriben después de nuestro mensaje separadas por coma, y
+   * se van rellenando en el lugar donde hemos puesto %X, siendo X un caracter
+   * el cual dependerá del tipo de valor a mostrar (En la siguiente sesión)
+   */
 
   if(n == 0){ // (1)
     fprintf(stderr, "Uso: fact N (N > 0)\n"); //stderr indica dónde quiero
--- Sesiones-C/Sesion02/hola.c
+++ Sesiones-C/Sesion02/hola.c
@@ -1,7 +1,7 @@
 /**
  * Sesion 02: Ejecutando y depurando C
  *
- * En esta sesión vamos a empezar ampliando nuestro 'hola.c' de la sesión
+ * En esta clase vamos a empezar ampliando nuestro 'hola.c' de la sesión
  * anterior. Vamos a ver cómo inicializar nuestro programa con diferentes
  * argumentos y cómo leerlos o saber el número que tiene.
  */
@@ -10,19 +10,20 @@
 
  int main(int argc, char *argv[]) {
    /**
-    * 'argc' es el número de comandos totales que el usuario introduce al
-    * ejecutar el programa, incluyendo el propipo programa:
-    * $ ./hola angel; (argc = 2)
+    * 'argc' es el número de argumentos totales que el usuario introduce al
+    * invocar el programa desde la linea de comandos, incluyendo el programa:
+    *
+    * $ ./hola angel (argc = 2, por tanto arg siempre será mayor que 0)
     */
 
   printf("El número de argumentos que hay es: %d\n", argc); // d = decimal
 
-  char *quien; // char *, significa en C un string
+  char *quien; // char *, significa por ahora una cadena de texto (un string)
 
-  if (argc == 1) { // Si hay un argumento, es decir, el programa en sí
+  if (argc == 1) { // Si hay un argumento, es decir, el programa sólo
     quien = "mundo";
   } else {
-    quien = argv[1];
+    quien = argv[1]; // argv[0] será siempre el nombre con el que se invoca
   }
 
   printf("¡Hola %s!\n", quien); // s = caracter
@@ -32,7 +33,7 @@
 
 /**
  * Recuerda que para compilar este fichero debes introducir:
- * $ gcc -o hola hola.c
+ * $ gcc hola.c -o hola
  *
  * La sesión continua en el archivo 'factorial.c', donde vamos a ver como,
  * escribiendo por la linea de comandos un número, nos devuelve el factorial.
